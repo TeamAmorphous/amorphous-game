70eadff16d79a2a0dda2aff4e6fa3584d93122ad
ParzivalVulpine <mrdaniomi36@gmail.com>
2024-06-10 14:12:00 -0500

    Cleaned NPC.gd

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -1,18 +1,17 @@
 extends StaticBody3D
 
+# Calls the speech sprite into a variable
 @onready var speech_sprite: Sprite3D = $Sprite3D/SpeechBubble
+# Movement Vector
 var vector_offset = Vector3(0, 0.02, 0)
-var up_dir := true
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  pass # Replace with function body.
+# Y-Coordinate limits
+const TOP_Y = 8.5
+const BOT_Y = 6.5
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 
-  print_debug(speech_sprite.position.y)
   speech_sprite.translate(vector_offset)
 
-  if speech_sprite.position.y >= 8.5 or speech_sprite.position.y <= 6.5:
+  # Flip the movement vector
+  if speech_sprite.position.y >= TOP_Y or speech_sprite.position.y <= BOT_Y:
     vector_offset *= -1
