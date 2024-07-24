69b742eb80bb444ac77200edfc5ef7eca91bdd21
ParzivalVulpine <dgaitan@calicofox.dev>
2024-07-24 15:45:42 -0500

    Refactored basic dialogue into level_controler.gd

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -1,50 +1,53 @@
 extends StaticBody3D
 
-# Calls the speech sprite into a variable
-@onready var speech_sprite: Sprite3D = $Sprite3D/SpeechBubble
+# TODO: Docstrings at some point
 
-# Passing off Skuldoo to then make him sit in place while talking
-var skuldoo_node : Node3D
-
-# Y-Coordinate limits
-const TOP_Y = 8.5
-const BOT_Y = 6.5
-
-#Speech bubble scaling
-const speech_scale_up := Vector3(1.3, 1.3, 1.3)
-# Magic numbers, I know... This is the inverse of 1.3 for scaling the speech
-# bubble prompt when you get close to an NPC
-const speech_scale_down := Vector3(0.76923077, 0.76923077, 0.76923077)
-
-@onready var dialogue_window : Panel = $"../Panel"
+# Level Controller Dialogue Signal
+signal lc_dialogue_initialized(text_paragraphs : Array)
 
 # NPC is interactable
 var player_interactable := false
+# Paragraph dialogue test
+var line1 := "line1"
+var line2 := "line2"
+var line3 := "line3"
+var paragraphs := [line1, line2, line3]
 
-# Level Controller Dialogue Signal
-signal lc_init_dialogue
+# Calls the speech sprite into a variable
+@onready var speech_sprite: Sprite3D = $Sprite3D/SpeechBubble
+@onready var dialogue_window : Panel = $"../Panel"
+
 
 func _process(_delta: float) -> void:
 
   if Input.is_action_just_pressed("interact") and player_interactable:
     print_debug("chat enabled!!!!")
-    # Lock skuldoo in place so that he doesn't leave a character talking alone
-    # he's not supossed to be rude...
-    skuldoo_node.in_dialogue = !skuldoo_node.in_dialogue
-    lc_init_dialogue.emit()
+    # TODO: Lock skuldoo in place so that he doesn't leave a character talking
+    # alone he's not supossed to be rude...
+
+    #skuldoo_node.in_dialogue = !skuldoo_node.in_dialogue
+    #dialogue_window.visible = !dialogue_window.visible
     print(dialogue_window)
-    dialogue_window.visible = !dialogue_window.visible
+    player_interactable = false
+    speech_sprite.visible = false
+    lc_dialogue_initialized.emit(paragraphs)
 
 # Scale up speech buble when the player is nearby
 func _on_talkable_area_body_entered(body:Node3D) -> void:
   print_debug(body.name)
-  if body.name == 'Skuldoo':
-    skuldoo_node = body
+  if body.name == "Skuldoo":
     speech_sprite.visible = true
     player_interactable = true
 
 # Scale down speech bubble when players goes away
 func _on_talkable_area_body_exited(body:Node3D) -> void:
-  if body.name == 'Skuldoo':
+  if body.name == "Skuldoo":
     speech_sprite.visible = false
     player_interactable = false
+
+func _on_level_controller_lc_dialogue_concluded() -> void:
+  print_debug("dialogue concluded")
+  await get_tree().create_timer(.1).timeout
+  player_interactable = true
+  speech_sprite.visible = true
+
--- scripts/level_controller.gd
+++ scripts/level_controller.gd
@@ -1,11 +1,36 @@
 extends Node
 
+# Future docstring tool?
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  pass # Replace with function body.
+signal lc_dialogue_concluded()
+
+var current_dialogue_content : Array
+var dialogue_paragraph_count := 0
+
+@onready var player_char : CharacterBody3D = $"../Skuldoo"
+@onready var main_dialogue_window : Label = $"../Panel/Label"
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-  pass
+func _on_flam_lc_dialogue_initialized(text_paragraphs:Array) -> void:
+  player_char.in_dialogue = true
+  main_dialogue_window.get_parent().visible = true
+  print_debug(text_paragraphs)
+  current_dialogue_content = text_paragraphs
+  main_dialogue_window.text = current_dialogue_content[dialogue_paragraph_count]
+
+func _process(_delta: float) -> void:
+  if Input.is_action_just_pressed("interact") and player_char.in_dialogue:
+    next_paragraph()
+
+func next_paragraph() -> void:
+  dialogue_paragraph_count += 1
+  if dialogue_paragraph_count == current_dialogue_content.size():
+    end_dialogue()
+  else:
+    main_dialogue_window.text = current_dialogue_content[dialogue_paragraph_count]
+
+func end_dialogue() -> void:
+  player_char.in_dialogue = false
+  main_dialogue_window.get_parent().visible = false
+  lc_dialogue_concluded.emit()
+  dialogue_paragraph_count = 0
