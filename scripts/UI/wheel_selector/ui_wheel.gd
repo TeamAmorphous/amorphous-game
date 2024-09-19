0680b2d305a4459c64e5e7908e0c90521488c47b
ParzivalVulpine <dgaitan@calicofox.dev>
2024-09-19 17:36:55 -0500

    Created additional signals for battle_controlled.gd

--- scripts/UI/wheel_selector/ui_wheel.gd
+++ scripts/UI/wheel_selector/ui_wheel.gd
@@ -1,3 +1,4 @@
+## TODO: Document code
 @tool
 extends Control
 
--- scripts/battle_controller.gd
+++ scripts/battle_controller.gd
@@ -7,20 +7,52 @@ extends Node
 ## communicate with one another while giving us the granular control of what
 ## events to trigger during gameplay.
 ##
+## The main loop of the battle breaks down into multiple signals that follow 
+## the main actions of the player, NPCs and enemies.
+## 
+## Consider that the process to follow is:
+## Battle Starts -> Show UI -> Player Action -> Enemy Action -> Battle Concludes
+##
+## Signals will be classified by the entity executing it, classified by the 
+## following:
+## - Player
+## - Enemy
+## - Scene
+##
+## Consider as well that when an attack happens (either from the player or an 
+## enemy). There is a two-step process, a signal to initiate the attack event
+## ("bc_player/enemy_attacked"), and a signal to signify the end of the minigame
+## ("bc_player/enemy_minigame_ended"). This will send back the results of the 
+## minigame in terms of damage done and rating bar fill.
+##
+## NOTE: In case we need to generate new signals within the battle controller,
+## keep the naming of them as "bc_(entity)_(action)"
+##
+## TODO: Define how to handle the fixed attack timer for enemies (Signal?)
+## TODO: Status effect system
+##
 ## @tutorial: https://www.gdquest.com/tutorial/godot/design-patterns/mediator/
 
-signal bc_battle_started(init: int)
-signal bc_ui_presented()
+signal bc_scene_battle_started(init: int)
+signal bc_scene_ui_presented()
+signal bc_scene_won(exp: int)
+signal bc_scene_lost()
+signal bc_concluded()
+
+# NOTE: Player action? or attack? Check naming later.
+signal bc_player_gear_switched()
+signal bc_player_item_used()
+signal bc_player_char_switched()
+signal bc_player_fled()
 signal bc_player_attacked()
-signal bc_player_minigame_ended(rating: float)
-signal bc_attack_bar_filled(atk_fill: int)
+
+signal bc_player_minigame_ended(rating: int)
+
 # TODO: Create a minigame class, or find a way to make templates for the 
 # different battle minigames in the game code.
-signal bc_enemy_attacked(minigame)
+signal bc_enemy_attacked()
 signal bc_enemy_minigame_ended(rating: float)
-signal bc_battle_won(exp: int)
-signal bc_battle_lost()
-signal bc_concluded()
+
 
 enum BattleInit {
   ENEMY,
@@ -30,15 +62,15 @@ enum BattleInit {
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  match GlobalVariables.current_battle_init:
-    BattleInit.ENEMY:
-      print_debug("enemy start")
-    BattleInit.NEUTRAL:
-      print_debug("neutral start")
-    BattleInit.PLAYER:
-      print_debug("player start")
-  bc_battle_started.emit(GlobalVariables.current_battle_init)
+	match GlobalVariables.current_battle_init:
+		BattleInit.ENEMY:
+			print_debug("enemy start")
+		BattleInit.NEUTRAL:
+			print_debug("neutral start")
+		BattleInit.PLAYER:
+			print_debug("player start")
+	bc_scene_battle_started.emit(GlobalVariables.current_battle_init)
   
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-  pass
+	pass
