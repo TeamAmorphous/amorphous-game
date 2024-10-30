ef0fea604df03a414b125fea17c427141d067325
ParzivalVulpine <dgaitan@calicofox.dev>
2024-10-30 12:25:41 -0500

    Sprite rendering on wheel selector component

--- scripts/UI/wheel_selector/ui_wheel.gd
+++ scripts/UI/wheel_selector/ui_wheel.gd
@@ -4,7 +4,7 @@ extends Control
 
 # TODO: Add item count on corner of sprite
 
-const SPRITE_SIZE = Vector2(32, 32)
+const SPRITE_SIZE = Vector2(96, 96)
 
 
 @export var background_color : Color = Color.BLACK
@@ -28,8 +28,14 @@ func _draw() -> void:
 	#TODO: Set code regions for function
 
 	# Gen algo:
-	# draw sprites (including highlighted brigtness)
-	# draw item count
+	# Draw Background Circle
+	# Draw Highlight Arc
+	# Draw Division Lines
+	# Draw Sprites (including highlighted brigtness)
+	# Draw Outer Circle Line
+	# Draw Inner Circle Line
+	# Draw item count
+	# Draw Hightlight Line
 
 	var offset = SPRITE_SIZE / -2
 
@@ -43,47 +49,49 @@ func _draw() -> void:
 		PackedColorArray([hightlight_color])
 		)
 
-	# Draw Lines
+	# Draw Division Lines and Sprites
 	for i in range(len(options)):
-		var rads = TAU * i / ((len(options) - 1))
-		var point = Vector2.from_angle(rads)
+		var angle = TAU * i / ((len(options)))
+		var line = Vector2.from_angle(angle)
 		draw_line(
-			point*(inner_radius+line_cutoff),
-			point*(outer_radius-line_cutoff),
+			line*(inner_radius+line_cutoff),
+			line*(outer_radius-line_cutoff),
 			line_color,
 			line_width
 			)
 
-	# draw_texture_rect_region(
-	# 	options[0].atlas,
-	# 	Rect2(offset, SPRITE_SIZE),
-	# 	options[0].region
-	# 	)
-
-	for i in range(1, len(options)):
-		pass
-
-	# Draw inner circle line
-	draw_arc(Vector2.ZERO, inner_radius, 0, TAU, 128, line_color, line_width, true)
-
-	# Draw outer circle line
-	draw_arc(Vector2.ZERO, outer_radius, 0, TAU, 128, line_color, line_width, true)
-
-	if len(options) >= 3:
-		for i in range(len(options) - 1):
-			var rads = TAU * i / ((len(options) - 1))
-			var point = Vector2.from_angle(rads)
-			draw_line(
-				point*(inner_radius+line_cutoff),
-				point*(outer_radius-line_cutoff),
-				line_color,
-				line_width
-				)
+		var start_rads = (TAU * (i-1) / (len(options)))
+		var end_rads = (TAU * i) / (len(options))
+		var mid_rads = (start_rads + end_rads) / 2.0 * -1
+		var radius_mid = (inner_radius + outer_radius) / 2
+		var draw_pos =  radius_mid * Vector2.from_angle(mid_rads) + offset
 		draw_texture_rect_region(
-			options[0].atlas,
-			Rect2(offset, SPRITE_SIZE),
-			options[0].region
+			options[i].atlas,
+			Rect2(draw_pos, SPRITE_SIZE),
+			options[i].region
 			)
+
+	# Draw Outer Circle Line
+	draw_arc(Vector2.ZERO, outer_radius, 0, TAU, 128, line_color, line_width)
+
+	# Draw Inner Circle Line
+	draw_arc(Vector2.ZERO, inner_radius, 0, TAU, 128, line_color, line_width)
+
+	# if len(options) >= 3:
+	# 	for i in range(len(options) - 1):
+	# 		var rads = TAU * i / ((len(options) - 1))
+	# 		var point = Vector2.from_angle(rads)
+	# 		draw_line(
+	# 			point*(inner_radius+line_cutoff),
+	# 			point*(outer_radius-line_cutoff),
+	# 			line_color,
+	# 			line_width
+	# 			)
+	# 	draw_texture_rect_region(
+	# 		options[0].atlas,
+	# 		Rect2(offset, SPRITE_SIZE),
+	# 		options[0].region
+	# 		)
 
 	#Draw Hightlight Line
 	draw_polyline(
@@ -100,14 +108,14 @@ func _process(_delta: float) -> void:
 		selection = 0
 	else:
 		var mouse_rads = fposmod(mouse_pos.angle() * -1, TAU)
-		selection = ceil((mouse_rads / TAU) * (len(options) - 1))
+		selection = ceil((mouse_rads / TAU) * (len(options)))
 
 	print(selection)
 	queue_redraw()
 
 func arc_calculation() -> PackedVector2Array:
-	var start_rads = (TAU * (selection-1) / (len(options) - 1))
-	var end_rads = (TAU * selection) / (len(options) - 1)
+	var start_rads = (TAU * (selection-1) / (len(options)))
+	var end_rads = (TAU * selection) / (len(options))
 	var points_per_arc = 32
 
 	var points_inner = PackedVector2Array()
